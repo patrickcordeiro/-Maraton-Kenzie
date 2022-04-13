3c2f7fffecbbc9ac947a0062e1cc26d2fcc45a8b
patrickcordeiro <patrick.cordeiro30@gmail.com>
2022-04-13 14:45:25 -0300

    Corrigindo alterações sugeridas pelo coach

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-
 function positions (firstPlace, secondPlace, lastPlace) {
     
     let podium = [firstPlace, secondPlace, lastPlace]
     let posicaoDaniel = podium.indexOf("Daniel")
     
-    let mural = " "
-
     if(posicaoDaniel > 0){
 
         let anteriorDaniel = podium[posicaoDaniel - 1]
         podium[posicaoDaniel - 1]  = "Daniel"
         podium[posicaoDaniel] = anteriorDaniel
-  
-        console.log(mural = `1°- Colocado ${podium[0]}, 2°- Colocado ${podium[1]}, 3°- Colocado ${podium[2]}.`)
-
-    }else {
-
-        console.log(mural = `1°- Colocado ${podium[0]}, 2°- Colocado ${podium[1]}, 3°- Colocado ${podium[2]}.`)     
 
     }
+
+    let mural = `1°- Colocado ${podium[0]}, 2°- Colocado ${podium[1]}, 3°- Colocado ${podium[2]}.`
+
+    return mural
 }
 
 //exemplos 
-positions("Daniel", "Maria", "Martha")
+console.log("Teste 1: " + positions("Daniel", "Maria", "Martha"))
 
-positions("Martha", "Daniel", "Maria")
+console.log("Teste 2: " + positions("Martha", "Daniel", "Maria"))
 
-positions("Maria", "Martha", "Daniel")
+console.log("Teste 3: " + positions("Maria", "Martha", "Daniel"))
